4ced895a0e3a0289c9cd31cc6031371cc9e27589
SengSerany <cranyseng@gmail.com>
2022-03-30 12:55:26 +0200

    better responses for mongo validation error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,4 @@
-const { CustomAPIError } = require('../errors')
+// const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   // console.log(err)
@@ -9,8 +9,13 @@ let customError = {
   msg: err.message || 'Something went wrong try again later'
 }
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // if (err instanceof CustomAPIError) {
+  //  return res.status(err.statusCode).json({ msg: err.message })
+  // }
+
+  if (err.name === "ValidationError"){
+    customError.msg = Object.values(err.errors).map((item) => item.message).join(', ');
+    customError.statusCode = 400;
   }
 
   if(err.code && err.code === 11000){
