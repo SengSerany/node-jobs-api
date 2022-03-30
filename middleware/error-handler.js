44c82998e5598dff45cd91ca66af984f52f9d9b6
SengSerany <cranyseng@gmail.com>
2022-03-30 12:41:21 +0200

    set mongo duplicate error func

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,10 +2,24 @@ const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   // console.log(err)
+
+let customError = {
+  //set default
+  statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+  msg: err.message || 'Something went wrong try again later'
+}
+
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if(err.code && err.code === 11000){
+    customError.msg = `The ${Object.keys(err.keyValue)} is already used, choose an other one or connect to this one.`;
+    customError.statusCode = 400;
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+  return res.status(customError.statusCode).json({ msg: customError.msg });
 }
 
 module.exports = errorHandlerMiddleware
