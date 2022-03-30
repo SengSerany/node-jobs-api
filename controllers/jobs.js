b28b14e13d3656eb9a9f9efc6ead7e2aa964e280
SengSerany <cranyseng@gmail.com>
2022-03-30 12:19:19 +0200

    set delete job func

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -42,7 +42,15 @@ const updateJob = async (req, res) => {
 };
 
 const deletejob = async (req, res) => {
-    res.send('delete job');
+    const { user: {userId}, params: {id: jobId} } = req;
+
+    const job = await Job.findOneAndRemove({  _id:jobId, createdBy: userId });
+
+    if(!job) {
+        throw new NotFoundError(`No job with th ID of ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
